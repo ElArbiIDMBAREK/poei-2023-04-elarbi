22e5720883d05b9504dca6845ae7faa371bfd3ab
ElArbi <elarbi.idmbarek@yahoo.com>
2023-04-03 14:54:27 +0200

    First commit

---
+++ src/test/java/AmazonTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+public class AmazonTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testAmazon() {
+        driver.get("https://www.amazon.fr");
+    }
+
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+}
